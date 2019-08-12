29b32c9edcc11d4a9d9d576e3f1e1912bd6a5ef4
Mehran <imganjgahi@gmail.com>
2019-08-12 18:22:54 +0430

    multi select initial files

--- src/Components/MultiSelect/MultiSelect.tsx
+++ src/Components/MultiSelect/MultiSelect.tsx
@@ -6,7 +6,7 @@ export interface IProps {
 }
  
 export interface IState {
-    displayValue: string;
+    displayValue: any[];
     searchValue: string;
     value: string;
     showOption: boolean;
@@ -17,10 +17,11 @@ export interface IState {
 class MultiSelect extends React.Component<IProps, IState> {
     ref: any;
     optionRef: any;
+    optionContainer: any;
     constructor(props: IProps) {
         super(props);
         this.state = {
-            displayValue: "",
+            displayValue: [],
             searchValue: "",
             value: "",
             showOption: false,
@@ -36,6 +37,7 @@ class MultiSelect extends React.Component<IProps, IState> {
         };
         this.ref = React.createRef()
         this.optionRef = React.createRef();
+        this.optionContainer = React.createRef();
     }
 
     handleClickOutside = (e: any) => {
@@ -45,15 +47,74 @@ class MultiSelect extends React.Component<IProps, IState> {
     }
     componentDidMount() {
         document.addEventListener("mousedown", this.handleClickOutside);
+        this.moveFocus()
     }
 
     componentWillUnmount() {
         document.removeEventListener("mousedown", this.handleClickOutside);
     }
+    _handleKeyDown = (e:any) =>{
+        if (e.key === 'Enter') {
+            const id = e.target.id;
+            console.log("ID: ", id)
+            const targetData = this.state.fakeData.filter(data => data.id === +id)[0]
+            this.onSelectHandler(targetData)
+          }
+    }
+    moveFocus() {
+        const node = this.optionContainer.current;
+        node.addEventListener('keydown', function(e: any) {
+          const active = document.activeElement;
+          if(active){
+            if(e.keyCode === 40 && active.nextSibling) {
+                (active.nextSibling as any).focus();
+              }
+              if(e.keyCode === 38 && active.previousSibling) {
+                (active.previousSibling as any).focus();
+              }
+          }
+        });
+      }
+      
+      optionHandler = (status: boolean) => {
+        this.setState({showOption: status, showInput: true, searchValue:""}, () => {
+            this.ref.current &&  this.ref.current.focus()
+        })
+    }
+
+      onSelectHandler = (data: any) => {
+          const displayList = this.state.displayValue;
+          const hasOne = this.state.displayValue.some(item => item.id === data.id);
+          if(!hasOne){
+              displayList.push(data)
+          }
+        this.setState({ value: data.id.toString(), displayValue: displayList, showOption: false, showInput: false, })
+    }
+
+    onChangeHandler = (event: any) => {
+        event.preventDefault();
+        this.setState({ searchValue: event.target.value })
+    }
     render() { 
         return ( 
-            <div>
-                multi
+            <div className="multiContainer" ref={this.optionRef}>
+                <div className="multiDisplayContainer" onClick={() => this.optionHandler(true)}>
+                    {this.state.displayValue.map((item, i) => {
+                        return (
+                            <div key={i} className="multiDisplayItem"> {item.title} </div>
+                        )
+                    })}
+                    <input ref={this.ref} type="text" className="multiSearchInput" onChange={this.onChangeHandler} value={this.state.searchValue} />
+                </div>
+                <div className="multiDataList" ref={this.optionContainer} tabIndex={1}>
+                    {this.state.fakeData.map((data, i) => {
+                        return (
+                            <div key={i} id={data.id} tabIndex={i} onKeyDown={this._handleKeyDown} className="selectOption" onClick={() => this.onSelectHandler(data)}>
+                                {data.title}
+                            </div>
+                        )
+                    })}
+                </div>
             </div>
          );
     }
