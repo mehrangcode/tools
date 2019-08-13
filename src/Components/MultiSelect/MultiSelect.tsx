3116e7b5e2627ed45907bfe08f3767bf71565ad5
Mehran <imganjgahi@gmail.com>
2019-08-13 11:17:52 +0430

    Debug: change event listners

--- src/Components/MultiSelect/MultiSelect.tsx
+++ src/Components/MultiSelect/MultiSelect.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import './MultiSelect.css';
 
 export interface IProps {
+    optionList?: any[];
+    url?: string;
     
 }
  
@@ -11,7 +13,7 @@ export interface IState {
     value: string;
     showOption: boolean;
     showInput: boolean;
-    fakeData: any[];
+    activeItem: number;
 }
  
 class MultiSelect extends React.Component<IProps, IState> {
@@ -26,23 +28,28 @@ class MultiSelect extends React.Component<IProps, IState> {
             value: "",
             showOption: false,
             showInput: true,
-            fakeData: [
-                { id: 1, title: "Red" },
-                { id: 2, title: "Green" },
-                { id: 3, title: "Blue" },
-                { id: 4, title: "Yellow" },
-                { id: 5, title: "Purple" },
-                { id: 6, title: "Orange" },
-            ]
+            activeItem: 0,
         };
         this.ref = React.createRef()
         this.optionRef = React.createRef();
         this.optionContainer = React.createRef();
     }
 
+    getOptions = (url: string) => {
+        console.log("getOptions", url)
+    }
+    componentWillReceiveProps(nextProps:any){
+        if(!this.props.url && nextProps.url){
+            this.getOptions(nextProps.url)
+        }
+    }
+    hideOption = () => {
+        this.setState({ showOption: false, searchValue:"", activeItem:0 });
+    }
+
     handleClickOutside = (e: any) => {
         if (this.optionRef.current && !this.optionRef.current.contains(e.target)) {
-            this.setState({ showOption: false, searchValue:"" });
+            this.hideOption()
         }
     }
     componentDidMount() {
@@ -55,24 +62,29 @@ class MultiSelect extends React.Component<IProps, IState> {
     }
     _handleKeyDown = (e:any) =>{
         if (e.key === 'Enter') {
-            const id = e.target.id;
-            console.log("ID: ", id)
-            const targetData = this.state.fakeData.filter(data => data.id === +id)[0]
-            this.onSelectHandler(targetData)
+            if(this.props.optionList){
+                const matchData = this.state.searchValue.toLocaleLowerCase().trim()
+               const datas= this.props.optionList.filter(data => data.title.toLocaleLowerCase().trim().match(matchData));
+                const id = datas[this.state.activeItem].id;
+                console.log("ID: ", id)
+                const targetData = datas.filter(data => data.id === +id)[0]
+                this.onSelectHandler(targetData)
+            }
           }
     }
     moveFocus() {
-        const node = this.optionContainer.current;
-        node.addEventListener('keydown', function(e: any) {
-          const active = document.activeElement;
-          if(active){
-            if(e.keyCode === 40 && active.nextSibling) {
-                (active.nextSibling as any).focus();
+        const node = this.ref.current;
+        const items: any[] = this.props.optionList ? this.props.optionList : []
+        node.addEventListener('keydown', (e: any) => {
+            let activeIndex = this.state.activeItem
+            if(e.keyCode === 40 && activeIndex < (items.length -1)) {
+                activeIndex++
               }
-              if(e.keyCode === 38 && active.previousSibling) {
-                (active.previousSibling as any).focus();
+              if(e.keyCode === 38 && activeIndex > 0) {
+                  activeIndex--
               }
-          }
+              
+            this.setState({activeItem: activeIndex})
         });
       }
       
@@ -88,12 +100,13 @@ class MultiSelect extends React.Component<IProps, IState> {
           if(!hasOne){
               displayList.push(data)
           }
+          this.ref.current &&  this.ref.current.focus()
         this.setState({ value: data.id.toString(), displayValue: displayList })
     }
 
     onChangeHandler = (event: any) => {
         event.preventDefault();
-        this.setState({ searchValue: event.target.value })
+        this.setState({ searchValue: event.target.value, activeItem: 0 })
     }
     unselectItem = (item: any) => {
         const selectedList = this.state.displayValue;
@@ -101,12 +114,11 @@ class MultiSelect extends React.Component<IProps, IState> {
         this.setState({displayValue: newSelectedList})
     }
     render() { 
-        let  datas= this.state.fakeData ? this.state.fakeData : [];
+        let  datas= this.props.optionList ? this.props.optionList : [];
         if(this.state.searchValue !=="" && datas.length > 0) {
-            const matchData = this.state.searchValue.toLocaleLowerCase()
-           datas= this.state.fakeData.filter(data => data.title.toLocaleLowerCase().match(matchData));
+            const matchData = this.state.searchValue.toLocaleLowerCase().trim()
+           datas= datas.filter(data => data.title.toLocaleLowerCase().trim().match(matchData));
         }
-        console.log("STATE: ", this.state)
         return ( 
             <div className="multiContainer" ref={this.optionRef}>
                 <div className="multiDisplayContainer" onClick={() => this.optionHandler(true)}>
@@ -119,15 +131,26 @@ class MultiSelect extends React.Component<IProps, IState> {
                         )
                     })}
                     <input
+                    tabIndex={0}
+                    // onBlur={this.hideOption}
                     style={{ display: this.state.showInput ? "block" : "none", 
                     width: this.state.searchValue.length === 0 ? "19px" :  (this.state.searchValue.length * 9)+"px"}}
-                    ref={this.ref} type="text" className="multiSearchInput" onChange={this.onChangeHandler} value={this.state.searchValue} />
+                    onKeyDown={this._handleKeyDown}
+                    ref={this.ref} type="text" className="multiSearchInput" 
+                    onChange={this.onChangeHandler} 
+                    value={this.state.searchValue} />
                 </div>
-                <div className={this.state.showOption ? "multiDataList" : "multiDataList optionHide"} ref={this.optionContainer} tabIndex={1}>
+                <div className={this.state.showOption ? "multiDataList" : "multiDataList optionHide"} ref={this.optionContainer} tabIndex={-1}>
                     {datas.map((data, i) => {
                         const isSelected = this.state.displayValue.some(selectedItem => selectedItem.id === data.id)
                         return (
-                            <div key={i} id={data.id} tabIndex={i} onKeyDown={this._handleKeyDown} className={isSelected ? " multiSelectOption multiSelectedItem" : "multiSelectOption"} onClick={() => this.onSelectHandler(data)}>
+                            <div key={i} id={data.id}  
+                            className={
+                                i === this.state.activeItem
+                                 ? "multiSelectOption multiOptionActive" :
+                                  isSelected ? " multiSelectOption multiSelectedItem" :
+                                   "multiSelectOption"} 
+                                   onClick={() => this.onSelectHandler(data)}>
                                 {data.title}
                             </div>
                         )
--- src/Components/Select/Select.tsx
+++ src/Components/Select/Select.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import './Select.css';
 export interface IProps {
-
+optionList?: any[];
+url?: string;
 }
 
 export interface IState {
@@ -10,7 +11,7 @@ export interface IState {
     value: string;
     showOption: boolean;
     showInput: boolean;
-    fakeData: any[];
+    activeItem: number;
 }
 
 class Select extends React.Component<IProps, IState> {
@@ -25,23 +26,34 @@ class Select extends React.Component<IProps, IState> {
             value: "",
             showOption: false,
             showInput: true,
-            fakeData: [
-                { id: 1, title: "Red" },
-                { id: 2, title: "Green" },
-                { id: 3, title: "Blue" },
-                { id: 4, title: "Yellow" },
-                { id: 5, title: "Purple" },
-                { id: 6, title: "Orange" },
-            ]
+            activeItem: 0,
         };
         this.ref = React.createRef()
         this.optionRef = React.createRef();
         this.optionContainer = React.createRef();
     }
 
+    getOptions = (url: string) => {
+        console.log("getOptions", url)
+    }
+    componentWillReceiveProps(nextProps:any){
+        if(!this.props.url && nextProps.url){
+            this.getOptions(nextProps.url)
+        }
+    }
+
+    hideOption = () => {
+        let {displayValue, showInput} = this.state
+            if(this.state.value !=="" && this.props.optionList) {
+                displayValue = this.props.optionList.filter(item=> item.id === +this.state.value)[0].title;
+            } 
+            showInput = false;
+            this.setState({ showOption: false, showInput ,displayValue });
+    }
+
     handleClickOutside = (e: any) => {
         if (this.optionRef.current && !this.optionRef.current.contains(e.target)) {
-            this.setState({ showOption: false });
+            this.hideOption()
         }
     }
     componentDidMount() {
@@ -63,48 +75,57 @@ class Select extends React.Component<IProps, IState> {
 
     onChangeHandler = (event: any) => {
         event.preventDefault();
-        this.setState({ searchValue: event.target.value, displayValue: "" })
+        this.setState({ searchValue: event.target.value, displayValue: "", activeItem: 0 })
     }
     _handleKeyDown = (e:any) =>{
         if (e.key === 'Enter') {
-            const id = e.target.id;
-            console.log("ID: ", id)
-            const targetData = this.state.fakeData.filter(data => data.id === +id)[0]
-            this.onSelectHandler(targetData)
+            if(this.props.optionList){
+                const matchData = this.state.searchValue.toLocaleLowerCase().trim()
+               const datas= this.props.optionList.filter(data => data.title.toLocaleLowerCase().trim().match(matchData));
+                const id = datas[this.state.activeItem].id;
+                console.log("ID: ", id)
+                const targetData = datas.filter(data => data.id === +id)[0]
+                this.onSelectHandler(targetData)
+            }
           }
     }
     onSelectHandler = (data: any) => {
-        this.setState({ value: data.id.toString(), displayValue: data.title, showOption: false, showInput: false, })
+        this.setState({ value: data.id.toString(), displayValue: data.title, showOption: false, showInput: false, activeItem:0 })
     }
-    moveFocus() {
-        const node = this.optionContainer.current;
-        node.addEventListener('keydown', function(e: any) {
-          const active = document.activeElement;
-          if(active){
-            if(e.keyCode === 40 && active.nextSibling) {
-                (active.nextSibling as any).focus();
+
+    moveFocus = () => {
+        const node = this.ref.current;
+        const items: any[] = this.props.optionList ? this.props.optionList : []
+        node.addEventListener('keydown', (e: any) => {
+            let activeIndex = this.state.activeItem
+            if(e.keyCode === 40 && activeIndex < (items.length -1)) {
+                activeIndex++
               }
-              if(e.keyCode === 38 && active.previousSibling) {
-                (active.previousSibling as any).focus();
+              if(e.keyCode === 38 && activeIndex > 0) {
+                  activeIndex--
               }
-          }
+              
+            this.setState({activeItem: activeIndex})
         });
       }
     render() {
-        let  datas= this.state.fakeData ? this.state.fakeData : [];
+        let  datas= this.props.optionList ? this.props.optionList : [];
         if(this.state.searchValue !=="" && datas.length > 0) {
-            const matchData = this.state.searchValue.toLocaleLowerCase()
-           datas= this.state.fakeData.filter(data => data.title.toLocaleLowerCase().match(matchData));
+            const matchData = this.state.searchValue.toLocaleLowerCase().trim()
+           datas= datas.filter(data => data.title.toLocaleLowerCase().trim().match(matchData));
         }
         console.log("STATE: ", this.state)
         return (
             <div className="selecContainer" ref={this.optionRef}>
                 <div className="displayContainer" onClick={() => this.optionHandler(true)}>
                     <div className="inputContainer">
-                        <input className="selectInput" ref={this.ref}
+                        <input 
+                        tabIndex={1}
+                         className="selectInput" ref={this.ref} onKeyDown={this._handleKeyDown}
+                        onBlur={this.hideOption}
                         style={{ display: this.state.showInput ? "block" : "none", 
                             width: this.state.searchValue.length === 0 ? "19px" :  (this.state.searchValue.length * 9)+"px"}}
-                         type="text" onChange={(event) => this.onChangeHandler(event)} value={this.state.searchValue} placeholder={this.state.displayValue ==="" ? "Search..." : ""} />
+                         type="text" onChange={(event) => this.onChangeHandler(event)} value={this.state.searchValue} />
                     </div>
                     <div className="selectDisplay">
                         {this.state.displayValue}
@@ -113,10 +134,10 @@ class Select extends React.Component<IProps, IState> {
                        \/
                     </div>
                 </div>
-                <div ref={this.optionContainer} tabIndex={1} className={this.state.showOption ? "optionContainer" : "optionContainer optionHide"}>
+                <div ref={this.optionContainer} tabIndex={-1} className={this.state.showOption ? "optionContainer" : "optionContainer optionHide"}>
                     {datas.map((data, i) => {
                         return (
-                            <div key={i} id={data.id} tabIndex={i} onKeyDown={this._handleKeyDown} className="selectOption" onClick={() => this.onSelectHandler(data)}>
+                            <div key={i} id={data.id} className={this.state.activeItem === i ? "selectOption activeOption" : "selectOption"} onClick={() => this.onSelectHandler(data)}>
                                 {data.title}
                             </div>
                         )
